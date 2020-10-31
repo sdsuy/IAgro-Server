ab5160ddeab9bd62aef0d726ad8a13c633ac2a5b
madgon <jose.madrid@estudiantes.utec.edu.uy>
2020-10-31 14:54:27 -0300

    Creo el Usuario con sus variables

--- ejbModule/com/entities/Usuario.java
+++ ejbModule/com/entities/Usuario.java
@@ -14,9 +14,98 @@ public class Usuario extends Base implements Serializable {
 
 	
 	private static final long serialVersionUID = 1L;
+	
+	@Column(length = 40)
+	private String nombre;
+	
+	@Column(length = 40)
+	private String apellido;
+	
+	@Column(length = 20)
+	private String documento;
+	
+	@Column(length = 20 )
+	private String clave;
+	
+	@Column(length = 40)
+	private String email;
+	
+	@ManyToOne(optional = false)
+	private Rol id_rol;
+	
 
 	public Usuario() {
 		super();
 	}
+
+	public Usuario(String nombre, String apellido, String documento, String clave, String email, Rol id_rol) {
+		super();
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.documento = documento;
+		this.clave = clave;
+		this.email = email;
+		this.id_rol = id_rol;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public String getApellido() {
+		return apellido;
+	}
+
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+
+	public String getDocumento() {
+		return documento;
+	}
+
+
+	public void setDocumento(String documento) {
+		this.documento = documento;
+	}
+
+
+	public String getClave() {
+		return clave;
+	}
+
+
+	public void setClave(String clave) {
+		this.clave = clave;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public Rol getId_rol() {
+		return id_rol;
+	}
+
+
+	public void setId_rol(Rol id_rol) {
+		this.id_rol = id_rol;
+	}
+	
    
 }
