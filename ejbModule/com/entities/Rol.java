5c514a085e711c9eeed904226295b7a97996a460
madgon <jose.madrid@estudiantes.utec.edu.uy>
2020-10-31 15:34:14 -0300

    Creada la entidad Rol con sus atributos

--- ejbModule/com/entities/Rol.java
+++ ejbModule/com/entities/Rol.java
@@ -1,6 +1,8 @@
 package com.entities;
 
 import java.io.Serializable;
+import java.util.List;
+
 import javax.persistence.*;
 
 /**
@@ -15,8 +17,49 @@ public class Rol extends Base implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 
+	@Column(length = 20)
+	private String nombre;
+	
+	@Column(length = 40)
+	private String descripcion;
+	
+	@ManyToMany
+	private List<Funcionalidad> funcionalidades;
+	
 	public Rol() {
 		super();
 	}
+
+	public Rol(String nombre, String descripcion, List<Funcionalidad> funcionalidades) {
+		super();
+		this.nombre = nombre;
+		this.descripcion = descripcion;
+		this.funcionalidades = funcionalidades;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public List<Funcionalidad> getFuncionalidades() {
+		return funcionalidades;
+	}
+
+	public void setFuncionalidades(List<Funcionalidad> funcionalidades) {
+		this.funcionalidades = funcionalidades;
+	}
    
+	
 }
