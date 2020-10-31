e5bce098518c67c58c7a3236e370acc12a692b2b
madgon <jose.madrid@estudiantes.utec.edu.uy>
2020-10-31 15:36:12 -0300

    Creada clase Funcionalidades

--- ejbModule/com/entities/Funcionalidad.java
+++ ejbModule/com/entities/Funcionalidad.java
@@ -14,9 +14,37 @@ public class Funcionalidad extends Base implements Serializable {
 
 	
 	private static final long serialVersionUID = 1L;
+	
+	@Column(length = 20)
+	private String nombre;
+	
+	@Column(length = 40)
+	private String descripcion;
 
 	public Funcionalidad() {
 		super();
+	}
+
+	public Funcionalidad(String nombre, String descripcion) {
+		super();
+		this.nombre = nombre;
+		this.descripcion = descripcion;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
 	}
    
 }
