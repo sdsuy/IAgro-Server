7fc46e720f07aec22555e196b5147233e53d50ec
madgon <jose.madrid@estudiantes.utec.edu.uy>
2020-11-01 13:38:20 -0300

    Genero los campos como documento, email, nickname como unicos

--- ejbModule/com/entities/Usuario.java
+++ ejbModule/com/entities/Usuario.java
@@ -22,16 +22,16 @@ public class Usuario extends Base implements Serializable {
 	@Column(length = 40, nullable = false)
 	private String apellido;
 	
-	@Column(length = 20)
+	@Column(length = 20, unique = true)
 	private String documento;
 	
 	@Column(length = 20, nullable = false)
 	private String clave;
 	
-	@Column(length = 40)
+	@Column(length = 40, nullable = false, unique = true)
 	private String email;
 	
-	@Column(length = 20, nullable = false)
+	@Column(length = 20, nullable = false, unique = true)
 	private String nickname;
 	
 	@ManyToOne(optional = false)
