8e0f0dfd6541bad91bb2c3e58a8b6b3a7db684d6
Santiago Da Silva <sdasilva@outlook.es>
2020-11-14 23:29:24 -0300

    Rol

--- ejbModule/com/entities/Rol.java
+++ ejbModule/com/entities/Rol.java
@@ -1,67 +1,7 @@
 package com.entities;
 
-import java.io.Serializable;
-import java.util.List;
-
-import javax.persistence.*;
-
-/**
- * Entity implementation class for Entity: Rol
- *
- */
-@Entity
-@Table(name="roles")
-@SequenceGenerator(name = "default_gen", sequenceName = "rol_seq", allocationSize = 1)
-@NamedQuery(name="Rol.readAll", query="SELECT r FROM Rol r")
-@NamedQuery(name="Rol.readfuncionalidades", query="SELECT DISTINCT r FROM Rol r LEFT JOIN FETCH r.funcionalidades f WHERE r.id = :id")
-public class Rol extends Base implements Serializable {
-
+public enum Rol {
 	
-	private static final long serialVersionUID = 1L;
+	 ADMINISTRADOR, EXPERTO, COMUN
 
-	@Column(length = 20, nullable = false, unique = true)
-	private String nombre;
-	
-	@Column(length = 40)
-	private String descripcion;
-	
-	@ManyToMany
-	private List<Funcionalidad> funcionalidades;
-	
-	public Rol() {
-		super();
-	}
-
-	public Rol(String nombre, String descripcion, List<Funcionalidad> funcionalidades) {
-		super();
-		this.nombre = nombre;
-		this.descripcion = descripcion;
-		this.funcionalidades = funcionalidades;
-	}
-
-	public String getNombre() {
-		return nombre;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
-	public String getDescripcion() {
-		return descripcion;
-	}
-
-	public void setDescripcion(String descripcion) {
-		this.descripcion = descripcion;
-	}
-
-	public List<Funcionalidad> getFuncionalidades() {
-		return funcionalidades;
-	}
-
-	public void setFuncionalidades(List<Funcionalidad> funcionalidades) {
-		this.funcionalidades = funcionalidades;
-	}
-   
-	
 }
