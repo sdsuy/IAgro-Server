5d283f46492d364946b2deeec982376b9f93fdf3
madgon <jose.madrid@estudiantes.utec.edu.uy>
2020-11-19 18:36:19 -0300

    Creo el servicio de Casillas

---
+++ ejbModule/com/service/CasillaBean.java
@@ -0,0 +1,18 @@
+package com.service;
+
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class CasillaBean
+ */
+@Stateless
+public class CasillaBean implements CasillaBeanRemote {
+
+    /**
+     * Default constructor. 
+     */
+    public CasillaBean() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
---
+++ ejbModule/com/service/CasillaBeanRemote.java
@@ -0,0 +1,10 @@
+package com.service;
+
+import javax.ejb.Remote;
+
+import com.entities.Casilla;
+
+@Remote
+public interface CasillaBeanRemote extends IBean<Casilla> {
+
+}
