7c64398204e49c0389b15fb9b56af45c74c00f6b
madgon <jose.madrid@estudiantes.utec.edu.uy>
2020-11-19 18:13:57 -0300

    FORMULARIO BEAN REMOTE

---
+++ ejbModule/com/service/FormularioBean.java
@@ -0,0 +1,18 @@
+package com.service;
+
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class FormularioBean
+ */
+@Stateless
+public class FormularioBean implements FormularioBeanRemote {
+
+    /**
+     * Default constructor. 
+     */
+    public FormularioBean() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
---
+++ ejbModule/com/service/FormularioBeanRemote.java
@@ -0,0 +1,12 @@
+package com.service;
+
+import javax.ejb.Remote;
+
+import com.entities.Formulario;
+
+@Remote
+public interface FormularioBeanRemote extends IBean<Formulario> {
+	
+	
+
+}
