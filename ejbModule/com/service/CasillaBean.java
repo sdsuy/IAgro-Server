5a2ea5d61e7870414834de7f744d6baa06271f2f
madgon <jose.madrid@estudiantes.utec.edu.uy>
2020-11-19 18:40:48 -0300

    Creo el remote de Casilla

--- ejbModule/com/service/CasillaBean.java
+++ ejbModule/com/service/CasillaBean.java
@@ -1,12 +1,21 @@
 package com.service;
 
+import java.util.List;
+
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
+
+import com.entities.Casilla;
+import com.persistence.CasillaDaoLocal;
 
 /**
  * Session Bean implementation class CasillaBean
  */
 @Stateless
 public class CasillaBean implements CasillaBeanRemote {
+	
+	@EJB
+	private CasillaDaoLocal casillaDao;
 
     /**
      * Default constructor. 
@@ -14,5 +23,30 @@ public class CasillaBean implements CasillaBeanRemote {
     public CasillaBean() {
         // TODO Auto-generated constructor stub
     }
+
+	@Override
+	public boolean create(Casilla o) {
+		return casillaDao.create(o);
+	}
+
+	@Override
+	public Casilla read(Long id) {
+		return casillaDao.read(id);
+	}
+
+	@Override
+	public List<Casilla> readAll() {
+		return casillaDao.readAll();
+	}
+
+	@Override
+	public boolean update(Casilla o) {
+		return casillaDao.update(o);
+	}
+
+	@Override
+	public boolean delete(Long id) {
+		return casillaDao.delete(id);
+	}
 
 }
