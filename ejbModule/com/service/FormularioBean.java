6d831d04f1bd9ee1eb565e9bb962d8e6de236491
madgon <jose.madrid@estudiantes.utec.edu.uy>
2020-11-19 18:17:04 -0300

    FormularioBean listo

--- ejbModule/com/service/FormularioBean.java
+++ ejbModule/com/service/FormularioBean.java
@@ -1,12 +1,21 @@
 package com.service;
 
+import java.util.List;
+
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
+
+import com.entities.Formulario;
+import com.persistence.FormularioDaoLocal;
 
 /**
  * Session Bean implementation class FormularioBean
  */
 @Stateless
 public class FormularioBean implements FormularioBeanRemote {
+	
+	@EJB
+	private FormularioDaoLocal formularioDao;
 
     /**
      * Default constructor. 
@@ -14,5 +23,35 @@ public class FormularioBean implements FormularioBeanRemote {
     public FormularioBean() {
         // TODO Auto-generated constructor stub
     }
+
+	@Override
+	public boolean create(Formulario o) {
+		
+		return formularioDao.create(o);
+	}
+
+	@Override
+	public Formulario read(Long id) {
+		
+		return formularioDao.read(id);
+	}
+
+	@Override
+	public List<Formulario> readAll() {
+		
+		return formularioDao.readAll();
+	}
+
+	@Override
+	public boolean update(Formulario o) {
+		
+		return formularioDao.update(o);
+	}
+
+	@Override
+	public boolean delete(Long id) {
+		
+		return formularioDao.delete(id);
+	}
 
 }
