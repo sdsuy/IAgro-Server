4acd2c943691af3cdc663941f0ce3d6af1938df0
madgon <jose.madrid@estudiantes.utec.edu.uy>
2020-11-19 18:09:31 -0300

    Lo dejo local

--- ejbModule/com/persistence/CasillaDaoLocal.java
+++ ejbModule/com/persistence/CasillaDaoLocal.java
@@ -1,10 +1,11 @@
 package com.persistence;
 
+import javax.ejb.Local;
 import javax.ejb.Remote;
 
 import com.entities.Casilla;
 
-@Remote
-public interface CasillaDaoRemote extends IDao<Casilla> {
+@Local
+public interface CasillaDaoLocal extends IDao<Casilla> {
 
 }
