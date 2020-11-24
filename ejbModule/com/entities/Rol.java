c228da8f30bf9c06f6579a47e4cd05be8b9c0a37
Santiago Da Silva <sdasilva@outlook.es>
2020-11-24 01:13:05 -0300

    entidad rol

--- ejbModule/com/entities/Rol.java
+++ ejbModule/com/entities/Rol.java
@@ -1,7 +1,34 @@
 package com.entities;
 
-public enum Rol {
-	
-	 ADMINISTRADOR, EXPERTO, COMUN
+import java.io.Serializable;
+import javax.persistence.*;
 
+/**
+ * Entity implementation class for Entity: Roles
+ *
+ */
+@Entity
+@Table(name="roles")
+@SequenceGenerator(name = "default_gen", sequenceName = "rol_seq", allocationSize = 1)
+public class Rol extends Base implements Serializable {
+
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Enumerated(EnumType.STRING)
+	private Roles rol;
+
+	public Rol() {
+		super();
+	}
+
+	public Roles getRol() {
+		return rol;
+	}
+
+	public void setRol(Roles rol) {
+		this.rol = rol;
+	}
+   
 }
+
