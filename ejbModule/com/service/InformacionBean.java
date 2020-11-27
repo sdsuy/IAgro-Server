1e3e8cbd293451de431db5d7925628a46fb19369
madgon <jose.madrid@estudiantes.utec.edu.uy>
2020-11-27 01:20:34 -0300

    InformacionBean

--- ejbModule/com/service/InformacionBean.java
+++ ejbModule/com/service/InformacionBean.java
@@ -1,12 +1,19 @@
 package com.service;
 
+import java.util.List;
+
 import javax.ejb.Stateless;
+
+import com.entities.Informacion;
+import com.persistence.InformacionDaoLocal;
 
 /**
  * Session Bean implementation class InformacionBean
  */
 @Stateless
 public class InformacionBean implements InformacionBeanRemote {
+	
+	private InformacionDaoLocal informacionDao;
 
     /**
      * Default constructor. 
@@ -14,5 +21,30 @@ public class InformacionBean implements InformacionBeanRemote {
     public InformacionBean() {
         // TODO Auto-generated constructor stub
     }
+
+	@Override
+	public boolean create(Informacion o) {
+		return informacionDao.create(o);
+	}
+
+	@Override
+	public Informacion read(Long id) {
+		return informacionDao.read(id);
+	}
+
+	@Override
+	public List<Informacion> readAll() {
+		return informacionDao.readAll();
+	}
+
+	@Override
+	public boolean update(Informacion o) {
+		return informacionDao.update(o);
+	}
+
+	@Override
+	public boolean delete(Long id) {
+		return informacionDao.delete(id);
+	}
 
 }
