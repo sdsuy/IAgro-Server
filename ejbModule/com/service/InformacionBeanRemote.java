c7207f9ba6ed5376f3a25ff92e36904c2917ea9a
madgon <jose.madrid@estudiantes.utec.edu.uy>
2020-11-27 01:17:32 -0300

    Agrego ibean

--- ejbModule/com/service/InformacionBeanRemote.java
+++ ejbModule/com/service/InformacionBeanRemote.java
@@ -2,7 +2,9 @@ package com.service;
 
 import javax.ejb.Remote;
 
+import com.entities.Informacion;
+
 @Remote
-public interface InformacionBeanRemote {
+public interface InformacionBeanRemote extends IBean<Informacion>{
 
 }
