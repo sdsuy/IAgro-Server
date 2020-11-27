6db456b5f3fae4812930be044677b9122de7a16d
madgon <jose.madrid@estudiantes.utec.edu.uy>
2020-11-27 00:54:56 -0300

    Creo la entidad Información

---
+++ ejbModule/com/entities/Informacion.java
@@ -0,0 +1,38 @@
+package com.entities;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+/**
+ * Entity implementation class for Entity: Informacion
+ *
+ */
+@Entity
+@Table(name="informacion")
+@SequenceGenerator(name = "default_gen", sequenceName = "info_seq", allocationSize = 1)
+public class Informacion extends Base implements Serializable  {
+
+	
+	private static final long serialVersionUID = 1L;
+	
+	@ManyToOne
+	private Casilla casilla;
+	
+	@ManyToOne
+	private Actividad actividad;
+	
+	private String valor;
+
+	public Informacion() {
+		super();
+	}
+
+	public String getValor() {
+		return valor;
+	}
+
+	public void setValor(String valor) {
+		this.valor = valor;
+	}
+   
+}
