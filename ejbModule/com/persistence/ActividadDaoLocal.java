ebb926772463ae05dedf443e8044a759b01c08d3
Santiago Da Silva <sdasilva@outlook.es>
2020-11-29 20:29:14 -0300

    interfaz del dao actividad con el nuevo metodo para filtrar por formulario

--- ejbModule/com/persistence/ActividadDaoLocal.java
+++ ejbModule/com/persistence/ActividadDaoLocal.java
@@ -1,5 +1,7 @@
 package com.persistence;
 
+import java.util.List;
+
 import javax.ejb.Local;
 
 import com.entities.Actividad;
@@ -7,5 +9,6 @@ import com.entities.Actividad;
 @Local
 public interface ActividadDaoLocal extends IDao<Actividad>{
 	
-
+	List<Actividad> readAllByForm(Long id);
+	
 }
